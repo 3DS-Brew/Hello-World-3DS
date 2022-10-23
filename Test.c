2ff2a5d99e5b9cb45e8c9310a47ab27aa6a673b6
Ty_1991 <67247243+Ty1991twitter@users.noreply.github.com>
2022-10-23 19:29:35 -0400

    Create Test.c

---
+++ Test.c
@@ -0,0 +1,11 @@
+#include "Test.cpp"
+#include <iostream>
+using namespace std
+
+int main()
+{
+    cout << "Hello World!"  << endl;
+    cout << "Just a copy of Test.cpp"  << endl;
+    system("pause")
+    return 0;
+}
