754205a2e397d4c669c2ce2c922dd87e1385e66f
UE4Pak <jamesmurray99k@gmail.com>
2022-12-22 15:58:05 +0000

    c:

---
+++ source/Test.cpp
@@ -0,0 +1,11 @@
+#include "Test.h"
+using namespace std
+
+int main()
+{
+    cout << "Hello World!"  << endl;
+    cout << "This is my hello world haxe project but somewhat remade in C++"  << endl;
+    cout << "Enjoy this dumb code of nothing-ness."  << endl;
+    system("pause")
+    return 0;
+}
---
+++ source/Test.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Main shits lmao
+#include <iostream>
+
+// 3DS shits
+#include <3ds.h>
+#include <citro3d.h>
+
+//Shader shits
+#include "vshader_shbin.h"
+
+// Application
+#include "graphics/Output.h"
